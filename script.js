f6bf427e7b761120917aa20069ae42fab94407c4
Mukil Elango <mukilane@gmail.com>
2016-11-25 15:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var app = angular.module('projects', ['ngMaterial', 'ngRoute']);
   app.config(function ($routeProvider) {
     $routeProvider
     .when("/", { templateUrl : "index.html" })
-    .when("/pomodoro", { templateUrl : "/pomodoro/index.html" })
-    .when("/simongame", { templateUrl : "/simongame/index.html" })
-    .when("/tctactoe", { templateUrl : "/tictactoe/index.html" })
-    .when("/calculator", { templateUrl : "/calculator/index.html" })
-    .when("/weather", { templateUrl : "/weather/index.html" })
-    .when("/twitchstatus", { templateUrl : "/twitchstatus/index.html" })
-    .when("/quoter", { templateUrl : "/quoter/index.html" })
-    .when("/wikisearch", { templateUrl : "/wikisearch/index.html" })
+    .when("/pomodoro", { templateUrl : "./pomodoro/index.html" })
+    .when("/simongame", { templateUrl : "./simongame/index.html" })
+    .when("/tctactoe", { templateUrl : "./tictactoe/index.html" })
+    .when("/calculator", { templateUrl : "./calculator/index.html" })
+    .when("/weather", { templateUrl : "./weather/index.html" })
+    .when("/twitchstatus", { templateUrl : "./twitchstatus/index.html" })
+    .when("/quoter", { templateUrl : "./quoter/index.html" })
+    .when("/wikisearch", { templateUrl : "./wikisearch/index.html" })
     .otherwise({ templateUrl : "index.html"});
   });
